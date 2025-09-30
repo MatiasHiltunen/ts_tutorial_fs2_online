a3755761e2f5b3e6b53488c18ed6ebd35c2679bb
matiashiltunen <26164588+MatiasHiltunen@users.noreply.github.com>
2025-09-30 20:09:48 +0300

    add example code

--- src/http.ts
+++ src/http.ts
@@ -1,12 +1,11 @@
 import {writeFile} from 'fs/promises'
 
-const imageUrl = "https://www.visitrovaniemi.fi/wp-content/uploads/Summer-and-midnight-sun-in-Rovaniemi-Lapland-Finland-7-800x450.jpg"
 
 const allowedMimetypes = ['image/jpeg', 'image/png']
 
-async function getImageFromUrl() {
+async function getImageFromUrl(url:URL) {
     
-    const response = await fetch(imageUrl)
+    const response = await fetch(url)
     
     const data = await response.blob()
 
@@ -16,8 +15,32 @@ async function getImageFromUrl() {
     
     return data
 
-    //await writeFile('test.jpg', data.stream())
+    
+}
+
+async function writeImage(imageName:string, imageData: Blob) {
+    
+    await writeFile(imageName, imageData.stream(), {
+        flush: true,
+
+    })
+}
+
+async function init() {
+
+    //const imageUrl = new URL("https://www.visitrovaniemi.fi/wp-content/uploads/Summer-and-midnight-sun-in-Rovaniemi-Lapland-Finland-7-800x450.jpg")
+    const imageUrl = new URL("https://www.visitrovaniemi.fi/wp-content/uploads/Santa-Claus-Village-in-Rovaniemi-Lapland-Finland-photo-by-Tommi-Lappalainen-SantaTV.jpg")
+
+
+    const imageName = imageUrl.pathname.split('/').at(-1) ?? imageUrl.pathname
+
+    const imageData = await getImageFromUrl(new URL(imageUrl))
+
+
+    await writeImage(imageName, imageData)
+
+
 
 }
 
-getImageFromUrl()
+init()
